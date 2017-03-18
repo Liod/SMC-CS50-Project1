f7f36b85f496f094d6aa3db1ef999684b8dc3e6e
Hunter Nakagawa <hunternakagawa@gmail.com>
2017-03-18 11:57:44 -0700

    initial push

---
+++ app.c
@@ -0,0 +1,42 @@
+/*
+@author Hunter Nakagawa
+@date March 17, 2017
+@class CS50, Prof. Paiola
+*/
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ACCOUNT_SIZE 10
+
+int helpmenu() {
+  printf("Banking v0\n");
+  printf("---------------------------------------\n");
+  printf("Available commands:\n");
+  printf("account - Start working with an account.\n");
+  printf("balance - Gets avaialble balance.\n");
+  printf("withdraw - Withdraw funds from an account.\n");
+  printf("deposit - Deposit funds into an account.\n");
+  printf("transfer - Transfer funds from one account into another account.\n");
+  printf("charge - Charge a balance to your account\n");
+  printf("check - Withdraw funds using a paper cheque.\n");
+  return 0;
+}
+int account(int number[]) {
+
+}
+int main() {
+  char menuselection[100];
+  // Handle the menu.
+  helpmenu();
+  while(1) {
+    gets(menuselection);
+    if(menuselection == "account") {
+      printf("...");
+    }
+    if(menuselection == "menu" || menuselection == "help") {
+      helpmenu();
+    }
+  }
+}
---
+++ test.bat
@@ -0,0 +1,2 @@
+cl app.c
+app
